d0026d1572fc80a2159958744376c824d0d30c1f
halynamecherzhak <galya.mech@gmail.com>
2019-02-12 17:17:02 +0200

    Added migrations files

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Controller;
+
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Exception\LogicException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Route("/register", name="user_registration")
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws |LogicException
+     */
+    public function registerAction(Request $request)
+    {
+
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user, []);
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $password = $password = $this
+                ->get('security.password_encoder')
+                ->encodePassword(
+                    $user,
+                    $user->getPlainPassword()
+                )
+            ;
+            $user->setPassword($password);
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success','You are now successfully register!');
+            //return $this->redirectToRoute('/home');
+
+        }
+
+        return $this->render(
+            'registration/register.html.twig',
+            array('form' => $form->createView())
+        );
+
+    }
+
+}
