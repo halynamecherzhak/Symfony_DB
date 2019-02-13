54de7cff002d07e771ad490be68955324d138962
halynamecherzhak <galya.mech@gmail.com>
2019-02-13 17:19:35 +0200

    LoginFormAuthenticator

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+    private $userRepository;
+    private $router;
+    public function __construct(UserRepository $userRepository, RouterInterface $router)
+    {
+        $this->userRepository = $userRepository;
+        $this->router = $router;
+    }
+    public function supports(Request $request)
+    {
+        // do your work when we're POSTing to the login page
+        return $request->attributes->get('_route') === 'app_login'
+            && $request->isMethod('POST');
+    }
+    public function getCredentials(Request $request)
+    {
+        return [
+            'email' => $request->request->get('email'),
+            'password' => $request->request->get('password'),
+        ];
+    }
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        return $this->userRepository->findOneBy(['email' => $credentials['email']]);
+    }
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // only needed if we need to check a password - we'll do that later!
+        return false;
+    }
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return new RedirectResponse($this->router->generate('app_homepage'));
+    }
+    protected function getLoginUrl()
+    {
+        // TODO: Implement getLoginUrl() method.
+        return $this->router->generate('app_login');
+    }
+}
