0fb8fe6c47b7727160d119b6d9d9187cddc8dfea
halynamecherzhak <galya.mech@gmail.com>
2019-02-18 14:09:45 +0200

    Created CRUD operations with Article entity

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Controller;
+
 use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,32 +11,40 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class ArticleController extends Controller {
+
     /**
      * @Route("/articles", name="article_list")
      * @Method({"GET"})
      */
+
     public function index() {
         $articles= $this->getDoctrine()->getRepository(Article::class)->findAll();
         return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
 
     /**
-     * @Route("/article/{id}", name="article_show")
+     * @Route("/article/delete/{id}", name="article_delete")
      */
 
-    //shows user description
-    public  function show($id)
+    public  function delete($id)
     {
-        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
-        return $this->render('articles/show.html.twig', array('article' => $article));
+        $em = $this->getDoctrine()->getManager();
 
+        $post = $em->getRepository(Article::class)->find($id);
+
+        $em->remove($post);
+        $em->flush();
+
+        return $this->redirectToRoute('article_list');
     }
 
     /**
      * @Route("/article/new", name="new_article")
      * Method({"GET", "POST"})
      */
+
     public function new(Request $request) {
         $article = new Article();
         $form = $this->createFormBuilder($article)
@@ -62,16 +72,60 @@ class ArticleController extends Controller {
     }
 
     /**
-     * @Route("/article/delete/{id}")
-     * @Method({"DELETE"})
+     * @Route("/article/{id}", name="article_show")
      */
-    public function delete(Request $request, $id) {
+
+    //shows user description
+    public  function show($id)
+    {
         $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->remove($article);
-        $entityManager->flush();
-        $response = new Response();
-        $response->send();
+        return $this->render('articles/show.html.twig', array('article' => $article));
     }
+
+    /**
+     * @Route("/article/edit/{id}", name="edit_article")
+     * Method({"GET", "POST"})
+     */
+
+    public function edit(Request $request, $id) {
+
+        $article = new Article();
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Update',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+        return $this->render('articles/edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("article/sort" , name="sort_article")
+     */
+
+    public  function  sort(){
+
+    }
+
 
 }
---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class MainController extends Controller {
+
+    /**
+     * @Route("/", name="home_page")
+     * @Method({"GET"})
+     */
+
+    public function index() {
+
+        return $this->render('/inc/mainNavBar.html.twig');
+    }
+}
